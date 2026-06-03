7fb20aa4aa71cabbdd2c533aea132dac53837878
Jan Čermák <sairon@sairon.cz>
2026-06-03 18:09:14 +0200

    Add Raspberry Pi Firmware update entity
    Create update entities for the Raspberry Pi firmware if Supervisor
    provides the API. The necessary OS Agent API will be added in Home
    Assistant OS 18 (needs latest dev currently) and requires Supervisor
    with home-assistant/supervisor#6886 (2026.06.0+, currently on beta).
    
    On some boards that do not support the update, Supervisor creates
    RPI_FIRMWARE_UPDATE_BLOCKED issue. This is currently not propagated to
    UI, and shows only in the CLI resolution center. This is because the set
    of boards that support the update may be extended and it'd be better to
    reduce the noise. In case the update is blocked, the update entity is
    simply not created now.
    
    After a successful update, the update entity optimistically shows the
    current version as the latest one and REBOOT_REQUIRED from Supervisor
    prompts user to reboot the board. In case the update isn't applied for
    some unknown reason, the update entity will reappear.
    
    aiohasupervisor bumped to v0.5.0:
    * https://github.com/home-assistant-libs/python-supervisor-client/releases/tag/0.5.0
      (added `feature_flags` in tests is side-effect of this bump)
    
    Refs home-assistant/operating-system#4631

--- tests/components/conftest.py
+++ tests/components/conftest.py
@@ -12,7 +12,11 @@ import string
 from typing import TYPE_CHECKING, Any
 from unittest.mock import AsyncMock, MagicMock, patch
 
-from aiohasupervisor import SupervisorClient, SupervisorNotFoundError
+from aiohasupervisor import (
+    SupervisorBadRequestError,
+    SupervisorClient,
+    SupervisorNotFoundError,
+)
 from aiohasupervisor.addons import AddonsClient
 from aiohasupervisor.backups import BackupsClient
 from aiohasupervisor.discovery import DiscoveryClient
@@ -722,6 +726,7 @@ def supervisor_info_fixture(supervisor_client: AsyncMock) -> AsyncMock:
         auto_update=True,
         country=None,
         detect_blocking_io=False,
+        feature_flags={},
     )
     return supervisor_client.supervisor.info
 
@@ -861,6 +866,10 @@ def supervisor_client() -> Generator[AsyncMock]:
     )
     supervisor_client.network = AsyncMock(spec=NetworkClient)
     supervisor_client.os = AsyncMock(spec=OSClient)
+    supervisor_client.os.raspberry_pi_firmware_info.side_effect = (
+        # default for most targets, to be overridden in RPi FW update tests
+        SupervisorBadRequestError
+    )
     supervisor_client.resolution = AsyncMock(spec=ResolutionClient)
     supervisor_client.supervisor = AsyncMock(spec=SupervisorManagementClient)
     supervisor_client.store = AsyncMock(spec=StoreClient)
